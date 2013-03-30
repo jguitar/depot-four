3f6f7dec2b7a7ee0192252b96a0f462c6b02b010
Juan F. Pérez <j.guitar@gmail.com>
2013-03-30 10:22:58 +0100

    adding functional testing to ProductsController

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -15,6 +15,9 @@ class ProductsControllerTest < ActionController::TestCase
     get :index
     assert_response :success
     assert_not_nil assigns(:products)
+    assert_select 'table tr', mininum: 3
+    assert_select 'table tr .list_actions', 3
+    assert_select 'a', 'New product'
   end
 
   test "should get new" do
