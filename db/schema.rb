78c29264d1f786604fa5cc22734cd013cf34aa35
Juan F. Pérez <j.guitar@gmail.com>
2013-05-05 18:29:34 +0200

    combine the sum of same products

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130406162636) do
+ActiveRecord::Schema.define(version: 20130505161929) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
