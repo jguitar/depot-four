d6b8ba67365210e6c53e48bc25f4bd59e71bf8cd
Juan F. Pérez <j.guitar@gmail.com>
2013-05-05 18:17:07 +0200

    add quantity to Cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -29,7 +29,7 @@ class LineItemsController < ApplicationController
   # POST /line_items.json
   def create
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
+    @line_item = @cart.add_product(product.id)
 
     reset_counter
 
